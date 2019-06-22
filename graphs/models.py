5d42a4520ba4e0b2de4e5b9a3d0267ce64921c39
John Meichle <jmeichle@gmail.com>
2019-06-22 01:59:35 -0400

    ok, base schema.

--- graphs/models.py
+++ graphs/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Graph(models.Model):
+    name = models.CharField(max_length=200)
+    created = models.DateTimeField('date published')
+
+class Node(models.Model):
+    graph = models.ForeignKey(Graph, on_delete=models.CASCADE)
+    value = models.CharField(max_length=200)
+
+class Edge(models.Model):
+    src_node = models.ForeignKey(Graph,
+        on_delete=models.PROTECT,
+        related_name='%(class)s_src_node'
+    )
+    dst_node = models.ForeignKey(Graph,
+        on_delete=models.PROTECT,
+        related_name='%(class)s_dst_node'
+    )
+
--- graphs/urls.py
+++ graphs/urls.py
@@ -1,4 +1,4 @@
-"""graphs_graphs_graphs URL Configuration
+"""graphs URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from . import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
 ]
